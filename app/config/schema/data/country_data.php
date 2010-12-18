775f758e852956f1d173646db4e6183e396b0a01
Firecreek <darren.m@firecreek.co.uk>
2010-12-18 17:36:22 +0700

    Croogo shell added Country data added

---
+++ app/config/schema/data/country_data.php
@@ -0,0 +1,998 @@
+<?php
+class CountryData {
+
+	public $table = 'countries';
+
+	public $records = array(
+		array(
+			'id' => '256',
+			'name' => 'Afghanistan'
+		),
+		array(
+			'id' => '257',
+			'name' => 'Albania'
+		),
+		array(
+			'id' => '258',
+			'name' => 'Algeria'
+		),
+		array(
+			'id' => '259',
+			'name' => 'American Samoa'
+		),
+		array(
+			'id' => '260',
+			'name' => 'Andorra'
+		),
+		array(
+			'id' => '261',
+			'name' => 'Angola'
+		),
+		array(
+			'id' => '262',
+			'name' => 'Anguilla'
+		),
+		array(
+			'id' => '263',
+			'name' => 'Antarctica'
+		),
+		array(
+			'id' => '264',
+			'name' => 'Antigua and Barbuda'
+		),
+		array(
+			'id' => '265',
+			'name' => 'Argentina'
+		),
+		array(
+			'id' => '266',
+			'name' => 'Armenia'
+		),
+		array(
+			'id' => '267',
+			'name' => 'Aruba'
+		),
+		array(
+			'id' => '268',
+			'name' => 'Australia'
+		),
+		array(
+			'id' => '269',
+			'name' => 'Austria'
+		),
+		array(
+			'id' => '270',
+			'name' => 'Azerbaijan'
+		),
+		array(
+			'id' => '271',
+			'name' => 'Bahamas'
+		),
+		array(
+			'id' => '272',
+			'name' => 'Bahrain'
+		),
+		array(
+			'id' => '273',
+			'name' => 'Bangladesh'
+		),
+		array(
+			'id' => '274',
+			'name' => 'Barbados'
+		),
+		array(
+			'id' => '275',
+			'name' => 'Belarus'
+		),
+		array(
+			'id' => '276',
+			'name' => 'Belgium'
+		),
+		array(
+			'id' => '277',
+			'name' => 'Belize'
+		),
+		array(
+			'id' => '278',
+			'name' => 'Benin'
+		),
+		array(
+			'id' => '279',
+			'name' => 'Bermuda'
+		),
+		array(
+			'id' => '280',
+			'name' => 'Bhutan'
+		),
+		array(
+			'id' => '281',
+			'name' => 'Bolivia'
+		),
+		array(
+			'id' => '282',
+			'name' => 'Bosnia and Herzegovina'
+		),
+		array(
+			'id' => '283',
+			'name' => 'Botswana'
+		),
+		array(
+			'id' => '284',
+			'name' => 'Bouvet Island'
+		),
+		array(
+			'id' => '285',
+			'name' => 'Brazil'
+		),
+		array(
+			'id' => '286',
+			'name' => 'British Indian Ocean Territory'
+		),
+		array(
+			'id' => '287',
+			'name' => 'Brunei Darussalam'
+		),
+		array(
+			'id' => '288',
+			'name' => 'Bulgaria'
+		),
+		array(
+			'id' => '289',
+			'name' => 'Burkina Faso'
+		),
+		array(
+			'id' => '290',
+			'name' => 'Burundi'
+		),
+		array(
+			'id' => '291',
+			'name' => 'Cambodia'
+		),
+		array(
+			'id' => '292',
+			'name' => 'Cameroon'
+		),
+		array(
+			'id' => '293',
+			'name' => 'Canada'
+		),
+		array(
+			'id' => '294',
+			'name' => 'Cape Verde'
+		),
+		array(
+			'id' => '295',
+			'name' => 'Cayman Islands'
+		),
+		array(
+			'id' => '296',
+			'name' => 'Central African Republic'
+		),
+		array(
+			'id' => '297',
+			'name' => 'Chad'
+		),
+		array(
+			'id' => '298',
+			'name' => 'Chile'
+		),
+		array(
+			'id' => '299',
+			'name' => 'China'
+		),
+		array(
+			'id' => '300',
+			'name' => 'Christmas Island'
+		),
+		array(
+			'id' => '301',
+			'name' => 'Cocos (Keeling) Islands'
+		),
+		array(
+			'id' => '302',
+			'name' => 'Colombia'
+		),
+		array(
+			'id' => '303',
+			'name' => 'Comoros'
+		),
+		array(
+			'id' => '304',
+			'name' => 'Congo'
+		),
+		array(
+			'id' => '305',
+			'name' => 'Congo, The Democratic Republic of the'
+		),
+		array(
+			'id' => '306',
+			'name' => 'Cook Islands'
+		),
+		array(
+			'id' => '307',
+			'name' => 'Costa Rica'
+		),
+		array(
+			'id' => '308',
+			'name' => 'Croatia'
+		),
+		array(
+			'id' => '309',
+			'name' => 'Cuba'
+		),
+		array(
+			'id' => '310',
+			'name' => 'Cyprus'
+		),
+		array(
+			'id' => '311',
+			'name' => 'Czech Republic'
+		),
+		array(
+			'id' => '312',
+			'name' => 'CÃ´te d'Ivoire'
+		),
+		array(
+			'id' => '313',
+			'name' => 'Denmark'
+		),
+		array(
+			'id' => '314',
+			'name' => 'Djibouti'
+		),
+		array(
+			'id' => '315',
+			'name' => 'Dominica'
+		),
+		array(
+			'id' => '316',
+			'name' => 'Dominican Republic'
+		),
+		array(
+			'id' => '317',
+			'name' => 'Ecuador'
+		),
+		array(
+			'id' => '318',
+			'name' => 'Egypt'
+		),
+		array(
+			'id' => '319',
+			'name' => 'El Salvador'
+		),
+		array(
+			'id' => '320',
+			'name' => 'Equatorial Guinea'
+		),
+		array(
+			'id' => '321',
+			'name' => 'Eritrea'
+		),
+		array(
+			'id' => '322',
+			'name' => 'Estonia'
+		),
+		array(
+			'id' => '323',
+			'name' => 'Ethiopia'
+		),
+		array(
+			'id' => '324',
+			'name' => 'Falkland Islands (Malvinas)'
+		),
+		array(
+			'id' => '325',
+			'name' => 'Faroe Islands'
+		),
+		array(
+			'id' => '326',
+			'name' => 'Fiji'
+		),
+		array(
+			'id' => '327',
+			'name' => 'Finland'
+		),
+		array(
+			'id' => '328',
+			'name' => 'France'
+		),
+		array(
+			'id' => '329',
+			'name' => 'French Guiana'
+		),
+		array(
+			'id' => '330',
+			'name' => 'French Polynesia'
+		),
+		array(
+			'id' => '331',
+			'name' => 'French Southern Territories'
+		),
+		array(
+			'id' => '332',
+			'name' => 'Gabon'
+		),
+		array(
+			'id' => '333',
+			'name' => 'Gambia'
+		),
+		array(
+			'id' => '334',
+			'name' => 'Georgia'
+		),
+		array(
+			'id' => '335',
+			'name' => 'Germany'
+		),
+		array(
+			'id' => '336',
+			'name' => 'Ghana'
+		),
+		array(
+			'id' => '337',
+			'name' => 'Gibraltar'
+		),
+		array(
+			'id' => '338',
+			'name' => 'Greece'
+		),
+		array(
+			'id' => '339',
+			'name' => 'Greenland'
+		),
+		array(
+			'id' => '340',
+			'name' => 'Grenada'
+		),
+		array(
+			'id' => '341',
+			'name' => 'Guadeloupe'
+		),
+		array(
+			'id' => '342',
+			'name' => 'Guam'
+		),
+		array(
+			'id' => '343',
+			'name' => 'Guatemala'
+		),
+		array(
+			'id' => '344',
+			'name' => 'Guernsey'
+		),
+		array(
+			'id' => '345',
+			'name' => 'Guinea'
+		),
+		array(
+			'id' => '346',
+			'name' => 'Guinea-Bissau'
+		),
+		array(
+			'id' => '347',
+			'name' => 'Guyana'
+		),
+		array(
+			'id' => '348',
+			'name' => 'Haiti'
+		),
+		array(
+			'id' => '349',
+			'name' => 'Heard Island and McDonald Islands'
+		),
+		array(
+			'id' => '350',
+			'name' => 'Holy See (Vatican City State)'
+		),
+		array(
+			'id' => '351',
+			'name' => 'Honduras'
+		),
+		array(
+			'id' => '352',
+			'name' => 'Hong Kong'
+		),
+		array(
+			'id' => '353',
+			'name' => 'Hungary'
+		),
+		array(
+			'id' => '354',
+			'name' => 'Iceland'
+		),
+		array(
+			'id' => '355',
+			'name' => 'India'
+		),
+		array(
+			'id' => '356',
+			'name' => 'Indonesia'
+		),
+		array(
+			'id' => '357',
+			'name' => 'Iran, Islamic Republic of'
+		),
+		array(
+			'id' => '358',
+			'name' => 'Iraq'
+		),
+		array(
+			'id' => '359',
+			'name' => 'Ireland'
+		),
+		array(
+			'id' => '360',
+			'name' => 'Isle of Man'
+		),
+		array(
+			'id' => '361',
+			'name' => 'Israel'
+		),
+		array(
+			'id' => '362',
+			'name' => 'Italy'
+		),
+		array(
+			'id' => '363',
+			'name' => 'Jamaica'
+		),
+		array(
+			'id' => '364',
+			'name' => 'Japan'
+		),
+		array(
+			'id' => '365',
+			'name' => 'Jersey'
+		),
+		array(
+			'id' => '366',
+			'name' => 'Jordan'
+		),
+		array(
+			'id' => '367',
+			'name' => 'Kazakhstan'
+		),
+		array(
+			'id' => '368',
+			'name' => 'Kenya'
+		),
+		array(
+			'id' => '369',
+			'name' => 'Kiribati'
+		),
+		array(
+			'id' => '370',
+			'name' => 'Korea, Democratic People's Republic of'
+		),
+		array(
+			'id' => '371',
+			'name' => 'Korea, Republic of'
+		),
+		array(
+			'id' => '372',
+			'name' => 'Kosovo, Republic of'
+		),
+		array(
+			'id' => '373',
+			'name' => 'Kuwait'
+		),
+		array(
+			'id' => '374',
+			'name' => 'Kyrgyzstan'
+		),
+		array(
+			'id' => '375',
+			'name' => 'Lao People's Democratic Republic'
+		),
+		array(
+			'id' => '376',
+			'name' => 'Latvia'
+		),
+		array(
+			'id' => '377',
+			'name' => 'Lebanon'
+		),
+		array(
+			'id' => '378',
+			'name' => 'Lesotho'
+		),
+		array(
+			'id' => '379',
+			'name' => 'Liberia'
+		),
+		array(
+			'id' => '380',
+			'name' => 'Libyan Arab Jamahiriya'
+		),
+		array(
+			'id' => '381',
+			'name' => 'Liechtenstein'
+		),
+		array(
+			'id' => '382',
+			'name' => 'Lithuania'
+		),
+		array(
+			'id' => '383',
+			'name' => 'Luxembourg'
+		),
+		array(
+			'id' => '384',
+			'name' => 'Macao'
+		),
+		array(
+			'id' => '385',
+			'name' => 'Macedonia, Republic of'
+		),
+		array(
+			'id' => '386',
+			'name' => 'Madagascar'
+		),
+		array(
+			'id' => '387',
+			'name' => 'Malawi'
+		),
+		array(
+			'id' => '388',
+			'name' => 'Malaysia'
+		),
+		array(
+			'id' => '389',
+			'name' => 'Maldives'
+		),
+		array(
+			'id' => '390',
+			'name' => 'Mali'
+		),
+		array(
+			'id' => '391',
+			'name' => 'Malta'
+		),
+		array(
+			'id' => '392',
+			'name' => 'Marshall Islands'
+		),
+		array(
+			'id' => '393',
+			'name' => 'Martinique'
+		),
+		array(
+			'id' => '394',
+			'name' => 'Mauritania'
+		),
+		array(
+			'id' => '395',
+			'name' => 'Mauritius'
+		),
+		array(
+			'id' => '396',
+			'name' => 'Mayotte'
+		),
+		array(
+			'id' => '397',
+			'name' => 'Mexico'
+		),
+		array(
+			'id' => '398',
+			'name' => 'Micronesia, Federated States of'
+		),
+		array(
+			'id' => '399',
+			'name' => 'Moldova'
+		),
+		array(
+			'id' => '400',
+			'name' => 'Monaco'
+		),
+		array(
+			'id' => '401',
+			'name' => 'Mongolia'
+		),
+		array(
+			'id' => '402',
+			'name' => 'Montenegro'
+		),
+		array(
+			'id' => '403',
+			'name' => 'Montserrat'
+		),
+		array(
+			'id' => '404',
+			'name' => 'Morocco'
+		),
+		array(
+			'id' => '405',
+			'name' => 'Mozambique'
+		),
+		array(
+			'id' => '406',
+			'name' => 'Myanmar'
+		),
+		array(
+			'id' => '407',
+			'name' => 'Namibia'
+		),
+		array(
+			'id' => '408',
+			'name' => 'Nauru'
+		),
+		array(
+			'id' => '409',
+			'name' => 'Nepal'
+		),
+		array(
+			'id' => '410',
+			'name' => 'Netherlands'
+		),
+		array(
+			'id' => '411',
+			'name' => 'Netherlands Antilles'
+		),
+		array(
+			'id' => '412',
+			'name' => 'New Caledonia'
+		),
+		array(
+			'id' => '413',
+			'name' => 'New Zealand'
+		),
+		array(
+			'id' => '414',
+			'name' => 'Nicaragua'
+		),
+		array(
+			'id' => '415',
+			'name' => 'Niger'
+		),
+		array(
+			'id' => '416',
+			'name' => 'Nigeria'
+		),
+		array(
+			'id' => '417',
+			'name' => 'Niue'
+		),
+		array(
+			'id' => '418',
+			'name' => 'Norfolk Island'
+		),
+		array(
+			'id' => '419',
+			'name' => 'Northern Mariana Islands'
+		),
+		array(
+			'id' => '420',
+			'name' => 'Norway'
+		),
+		array(
+			'id' => '421',
+			'name' => 'Oman'
+		),
+		array(
+			'id' => '422',
+			'name' => 'Pakistan'
+		),
+		array(
+			'id' => '423',
+			'name' => 'Palau'
+		),
+		array(
+			'id' => '424',
+			'name' => 'Palestinian Territory, Occupied'
+		),
+		array(
+			'id' => '425',
+			'name' => 'Panama'
+		),
+		array(
+			'id' => '426',
+			'name' => 'Papua New Guinea'
+		),
+		array(
+			'id' => '427',
+			'name' => 'Paraguay'
+		),
+		array(
+			'id' => '428',
+			'name' => 'Peru'
+		),
+		array(
+			'id' => '429',
+			'name' => 'Philippines'
+		),
+		array(
+			'id' => '430',
+			'name' => 'Pitcairn'
+		),
+		array(
+			'id' => '431',
+			'name' => 'Poland'
+		),
+		array(
+			'id' => '432',
+			'name' => 'Portugal'
+		),
+		array(
+			'id' => '433',
+			'name' => 'Puerto Rico'
+		),
+		array(
+			'id' => '434',
+			'name' => 'Qatar'
+		),
+		array(
+			'id' => '435',
+			'name' => 'Reunion'
+		),
+		array(
+			'id' => '436',
+			'name' => 'Romania'
+		),
+		array(
+			'id' => '437',
+			'name' => 'Russian Federation'
+		),
+		array(
+			'id' => '438',
+			'name' => 'Rwanda'
+		),
+		array(
+			'id' => '439',
+			'name' => 'Saint BarthÃ©lemy'
+		),
+		array(
+			'id' => '440',
+			'name' => 'Saint Helena'
+		),
+		array(
+			'id' => '441',
+			'name' => 'Saint Kitts and Nevis'
+		),
+		array(
+			'id' => '442',
+			'name' => 'Saint Lucia'
+		),
+		array(
+			'id' => '443',
+			'name' => 'Saint Martin (French part)'
+		),
+		array(
+			'id' => '444',
+			'name' => 'Saint Pierre and Miquelon'
+		),
+		array(
+			'id' => '445',
+			'name' => 'Saint Vincent and the Grenadines'
+		),
+		array(
+			'id' => '446',
+			'name' => 'Samoa'
+		),
+		array(
+			'id' => '447',
+			'name' => 'San Marino'
+		),
+		array(
+			'id' => '448',
+			'name' => 'Sao Tome and Principe'
+		),
+		array(
+			'id' => '449',
+			'name' => 'Saudi Arabia'
+		),
+		array(
+			'id' => '450',
+			'name' => 'Senegal'
+		),
+		array(
+			'id' => '451',
+			'name' => 'Serbia'
+		),
+		array(
+			'id' => '452',
+			'name' => 'Seychelles'
+		),
+		array(
+			'id' => '453',
+			'name' => 'Sierra Leone'
+		),
+		array(
+			'id' => '454',
+			'name' => 'Singapore'
+		),
+		array(
+			'id' => '455',
+			'name' => 'Slovakia'
+		),
+		array(
+			'id' => '456',
+			'name' => 'Slovenia'
+		),
+		array(
+			'id' => '457',
+			'name' => 'Solomon Islands'
+		),
+		array(
+			'id' => '458',
+			'name' => 'Somalia'
+		),
+		array(
+			'id' => '459',
+			'name' => 'South Africa'
+		),
+		array(
+			'id' => '460',
+			'name' => 'South Georgia and the South Sandwich Islands'
+		),
+		array(
+			'id' => '461',
+			'name' => 'Spain'
+		),
+		array(
+			'id' => '462',
+			'name' => 'Sri Lanka'
+		),
+		array(
+			'id' => '463',
+			'name' => 'Sudan'
+		),
+		array(
+			'id' => '464',
+			'name' => 'Suriname'
+		),
+		array(
+			'id' => '465',
+			'name' => 'Svalbard and Jan Mayen'
+		),
+		array(
+			'id' => '466',
+			'name' => 'Swaziland'
+		),
+		array(
+			'id' => '467',
+			'name' => 'Sweden'
+		),
+		array(
+			'id' => '468',
+			'name' => 'Switzerland'
+		),
+		array(
+			'id' => '469',
+			'name' => 'Syrian Arab Republic'
+		),
+		array(
+			'id' => '470',
+			'name' => 'Taiwan'
+		),
+		array(
+			'id' => '471',
+			'name' => 'Tajikistan'
+		),
+		array(
+			'id' => '472',
+			'name' => 'Tanzania, United Republic of'
+		),
+		array(
+			'id' => '473',
+			'name' => 'Thailand'
+		),
+		array(
+			'id' => '474',
+			'name' => 'Timor-Leste'
+		),
+		array(
+			'id' => '475',
+			'name' => 'Togo'
+		),
+		array(
+			'id' => '476',
+			'name' => 'Tokelau'
+		),
+		array(
+			'id' => '477',
+			'name' => 'Tonga'
+		),
+		array(
+			'id' => '478',
+			'name' => 'Trinidad and Tobago'
+		),
+		array(
+			'id' => '479',
+			'name' => 'Tunisia'
+		),
+		array(
+			'id' => '480',
+			'name' => 'Turkey'
+		),
+		array(
+			'id' => '481',
+			'name' => 'Turkmenistan'
+		),
+		array(
+			'id' => '482',
+			'name' => 'Turks and Caicos Islands'
+		),
+		array(
+			'id' => '483',
+			'name' => 'Tuvalu'
+		),
+		array(
+			'id' => '484',
+			'name' => 'Uganda'
+		),
+		array(
+			'id' => '485',
+			'name' => 'Ukraine'
+		),
+		array(
+			'id' => '486',
+			'name' => 'United Arab Emirates'
+		),
+		array(
+			'id' => '487',
+			'name' => 'United Kingdom'
+		),
+		array(
+			'id' => '488',
+			'name' => 'United States'
+		),
+		array(
+			'id' => '489',
+			'name' => 'United States Minor Outlying Islands'
+		),
+		array(
+			'id' => '490',
+			'name' => 'Uruguay'
+		),
+		array(
+			'id' => '491',
+			'name' => 'Uzbekistan'
+		),
+		array(
+			'id' => '492',
+			'name' => 'Vanuatu'
+		),
+		array(
+			'id' => '493',
+			'name' => 'Venezuela'
+		),
+		array(
+			'id' => '494',
+			'name' => 'Viet Nam'
+		),
+		array(
+			'id' => '495',
+			'name' => 'Virgin Islands, British'
+		),
+		array(
+			'id' => '496',
+			'name' => 'Virgin Islands, U.S.'
+		),
+		array(
+			'id' => '497',
+			'name' => 'Wallis and Futuna'
+		),
+		array(
+			'id' => '498',
+			'name' => 'Western Sahara'
+		),
+		array(
+			'id' => '499',
+			'name' => 'Yemen'
+		),
+		array(
+			'id' => '500',
+			'name' => 'Zambia'
+		),
+		array(
+			'id' => '501',
+			'name' => 'Zimbabwe'
+		),
+		array(
+			'id' => '502',
+			'name' => 'Ã…land Islands'
+		),
+	);
+
+}
+?>
