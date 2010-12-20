0d0d3b8bc19f94c9562bd485fd80de3870f7797f
Firecreek <darren.m@firecreek.co.uk>
2010-12-20 20:39:46 +0700

    Scheme data

---
+++ app/config/schema/data/scheme_data.php
@@ -0,0 +1,28 @@
+<?php
+class SchemeData {
+
+	public $table = 'schemes';
+
+	public $records = array(
+		array(
+			'id' => '1',
+			'name' => 'Default',
+			'position' => '1',
+			'created' => '2010-12-20 20:31:48'
+		),
+		array(
+			'id' => '2',
+			'name' => 'Winter',
+			'position' => '2',
+			'created' => '2010-12-20 20:31:48'
+		),
+		array(
+			'id' => '3',
+			'name' => 'Burning',
+			'position' => '3',
+			'created' => '2010-12-20 20:32:22'
+		),
+	);
+
+}
+?>
---
+++ app/config/schema/data/schemes_style_data.php
@@ -0,0 +1,172 @@
+<?php
+class SchemesStyleData {
+
+	public $table = 'schemes_styles';
+
+	public $records = array(
+		array(
+			'id' => '1',
+			'scheme_id' => '1',
+			'key' => 'backgroundColor',
+			'value' => '#195D00'
+		),
+		array(
+			'id' => '2',
+			'scheme_id' => '1',
+			'key' => 'projectTextColour',
+			'value' => '#FFFFF'
+		),
+		array(
+			'id' => '3',
+			'scheme_id' => '1',
+			'key' => 'clientTextColour',
+			'value' => '#FFFFF'
+		),
+		array(
+			'id' => '4',
+			'scheme_id' => '1',
+			'key' => 'tabBackground',
+			'value' => '#217A00'
+		),
+		array(
+			'id' => '5',
+			'scheme_id' => '1',
+			'key' => 'tabBackgroundHover',
+			'value' => '#054700'
+		),
+		array(
+			'id' => '6',
+			'scheme_id' => '1',
+			'key' => 'tabTextColour',
+			'value' => '#D1EEFF'
+		),
+		array(
+			'id' => '7',
+			'scheme_id' => '1',
+			'key' => 'tabTextColourHover',
+			'value' => '#FFFFF'
+		),
+		array(
+			'id' => '8',
+			'scheme_id' => '1',
+			'key' => 'tabTextColourActive',
+			'value' => '#000000'
+		),
+		array(
+			'id' => '9',
+			'scheme_id' => '1',
+			'key' => 'linkTextColour',
+			'value' => '#03C'
+		),
+		array(
+			'id' => '10',
+			'scheme_id' => '2',
+			'key' => 'backgroundColor',
+			'value' => '#003366'
+		),
+		array(
+			'id' => '11',
+			'scheme_id' => '2',
+			'key' => 'projectTextColour',
+			'value' => '#ffcc00'
+		),
+		array(
+			'id' => '12',
+			'scheme_id' => '2',
+			'key' => 'clientTextColour',
+			'value' => '#ffffff'
+		),
+		array(
+			'id' => '13',
+			'scheme_id' => '2',
+			'key' => 'tabBackground',
+			'value' => '#1a4f85'
+		),
+		array(
+			'id' => '14',
+			'scheme_id' => '2',
+			'key' => 'tabBackgroundHover',
+			'value' => '#1d64ad'
+		),
+		array(
+			'id' => '15',
+			'scheme_id' => '2',
+			'key' => 'tabTextColour',
+			'value' => '#ffffff'
+		),
+		array(
+			'id' => '16',
+			'scheme_id' => '2',
+			'key' => 'tabTextColourHover',
+			'value' => '#ffffff'
+		),
+		array(
+			'id' => '17',
+			'scheme_id' => '2',
+			'key' => 'tabTextColourActive',
+			'value' => '#009900'
+		),
+		array(
+			'id' => '18',
+			'scheme_id' => '2',
+			'key' => 'linkTextColour',
+			'value' => '#0033cc'
+		),
+		array(
+			'id' => '19',
+			'scheme_id' => '3',
+			'key' => 'backgroundColor',
+			'value' => '#990000'
+		),
+		array(
+			'id' => '20',
+			'scheme_id' => '3',
+			'key' => 'projectTextColour',
+			'value' => '#ffcc00'
+		),
+		array(
+			'id' => '21',
+			'scheme_id' => '3',
+			'key' => 'clientTextColour',
+			'value' => '#ffffff'
+		),
+		array(
+			'id' => '22',
+			'scheme_id' => '3',
+			'key' => 'tabBackground',
+			'value' => '#cc0000'
+		),
+		array(
+			'id' => '23',
+			'scheme_id' => '3',
+			'key' => 'tabBackgroundHover',
+			'value' => '#660000'
+		),
+		array(
+			'id' => '24',
+			'scheme_id' => '3',
+			'key' => 'tabTextColour',
+			'value' => '#ffffff'
+		),
+		array(
+			'id' => '25',
+			'scheme_id' => '3',
+			'key' => 'tabTextColourHover',
+			'value' => '#ffcc00'
+		),
+		array(
+			'id' => '26',
+			'scheme_id' => '3',
+			'key' => 'tabTextColourActive',
+			'value' => '#990000'
+		),
+		array(
+			'id' => '27',
+			'scheme_id' => '3',
+			'key' => 'linkTextColour',
+			'value' => '#0033cc'
+		),
+	);
+
+}
+?>
