bf4ebd623edf361b051acdfda7ea7db1ee3f8ff0
Firecreek <darren.m@firecreek.co.uk>
2010-12-30 13:41:29 +0700

    PHPdoc

--- app/views/helpers/auth.php
+++ app/views/helpers/auth.php
@@ -12,13 +12,36 @@
    */
   class AuthHelper extends AppHelper
   {
+    /**
+     * Helpers
+     *
+     * @access public
+     * @var array
+     */
     public $helpers = array('Session');
     
+    
+    /**
+     * Check permission
+     *
+     * @param string $alias ACO alias
+     * @param string $type create, update, permission type
+     * @access public
+     * @return boolean
+     */
     public function check($alias,$type)
     {
       return $this->Session->read('AuthAccount.Permissions.'.$alias.'.'.$type);
     }
     
+    
+    /**
+     * Read session key
+     *
+     * @param string $key
+     * @access public
+     * @return string
+     */
     public function read($key)
     {
       return $this->Session->read('AuthAccount.'.$key);
