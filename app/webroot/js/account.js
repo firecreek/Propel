33e022b27a089c5a39ad7230896a62474bf6774c
Firecreek <darren.m@firecreek.co.uk>
2011-02-17 18:49:46 +0700

    wysiwyg editor for posts - needs finishing off with active button states

--- app/webroot/js/account.js
+++ app/webroot/js/account.js
@@ -3,6 +3,18 @@ var Account = {
   
   load: function()
   {
+    this._wysiwyg();
+  },
+  
+  _wysiwyg: function()
+  {
+    if($('.wysiwyg').length > 0)
+    {
+      $('.wysiwyg').rte({
+        media_url:'/img/rte/',
+        content_css_url:'/css/rte.css'
+      });
+    }
   }
 
 }
