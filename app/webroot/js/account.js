c0de7cf4d99a51ab44982364107aaeef6df4145e
Firecreek <darren.m@firecreek.co.uk>
2011-02-28 00:42:17 +0700

    Session flash, if success then hide after a few seconds

--- app/webroot/js/account.js
+++ app/webroot/js/account.js
@@ -3,14 +3,28 @@ var Account = {
   
   load: function()
   {
+    this._menus();
+    this._flash();
+    this._wysiwyg();
+  },
+  
+  _menus: function()
+  {
     $("ul.sf-menu").superfish({
       autoArrows: false,
       speed: 400,
       delay: 500,
       delayShow: 300
     });
-    
-    this._wysiwyg();
+  },
+  
+  _flash: function()
+  {
+    if($('#flashMessage.success'))
+    {
+      setTimeout(function() { $('#flashMessage.success').effect('highlight'); },500);
+      setTimeout(function() { $('#flashMessage.success').fadeOut(); },3000);
+    }
   },
   
   _wysiwyg: function()
