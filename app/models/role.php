1a29aa0175c598b2d30a20cf2ea5c71bb522a3d9
Firecreek <darren.m@firecreek.co.uk>
2011-05-01 23:42:07 +0700

    PHPDoc clean up

--- app/models/role.php
+++ app/models/role.php
@@ -1,45 +1,85 @@
 <?php
-class Role extends AppModel {
-	var $name = 'Role';
-	var $validate = array(
-		'name' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-	
+
+  /**
+   * Role Model
+   *
+   * @category Model
+   * @package  Propel
+   * @version  1.0
+   * @author   Darren Moore <darren.m@firecreek.co.uk>
+   * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+   * @link     http://www.propelhq.com
+   */
+  class Role extends AppModel
+  {
+    /**
+     * Name of model
+     *
+     * @access public
+     * @var string
+     */
+    public $name = 'Role';
+    
+    /**
+     * Validation rules
+     *
+     * @access public
+     * @var array
+     */
+    public $validate = array(
+      'name' => array(
+        'notempty' => array(
+          'rule' => array('notempty')
+        ),
+      ),
+    );
+    
+    /**
+     * Behaviors
+     *
+     * @access public
+     * @var array
+     */
     public $actsAs = array(
         'Acl' => array(
             'type' => 'requester',
         ),
     );
 
-	var $hasMany = array(
-		'User' => array(
-			'className' => 'User',
-			'foreignKey' => 'role_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+    /**
+     * Has many
+     *
+     * @access public
+     * @var array
+     */
+    public $hasMany = array(
+      'User' => array(
+        'className' => 'User',
+        'foreignKey' => 'role_id',
+        'dependent' => false,
+        'conditions' => '',
+        'fields' => '',
+        'order' => '',
+        'limit' => '',
+        'offset' => '',
+        'exclusive' => '',
+        'finderQuery' => '',
+        'counterQuery' => ''
+      )
+    );
 
-    public function parentNode() {
-        return null;
+    
+    /**
+     * Parent node
+     *
+     * @access public
+     * @return null
+     */
+    public function parentNode()
+    {
+      return null;
     }
-		
-}
+      
+  }
+  
 ?>
--- app/models/timezone.php
+++ app/models/timezone.php
@@ -1,48 +1,58 @@
 <?php
-class Timezone extends AppModel {
-	var $name = 'Timezone';
-	var $validate = array(
-		'name' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
-	var $hasMany = array(
-		'Company' => array(
-			'className' => 'Company',
-			'foreignKey' => 'timezone_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		),
-		'User' => array(
-			'className' => 'User',
-			'foreignKey' => 'timezone_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+  /**
+   * Timezone Model
+   *
+   * @category Model
+   * @package  Propel
+   * @version  1.0
+   * @author   Darren Moore <darren.m@firecreek.co.uk>
+   * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+   * @link     http://www.propelhq.com
+   */
+  class Timezone extends AppModel
+  {
+    /**
+     * Name of model
+     *
+     * @access public
+     * @var string
+     */
+    public $name = 'Timezone';
+    
+    /**
+     * Validation rules
+     *
+     * @access public
+     * @var array
+     */
+    public $validate = array(
+      'name' => array(
+        'notempty' => array(
+          'rule' => array('notempty')
+        ),
+      ),
+    );
 
-}
+    /**
+     * Has many
+     *
+     * @access public
+     * @var array
+     */
+    public $hasMany = array(
+      'Company' => array(
+        'className' => 'Company',
+        'foreignKey' => 'timezone_id',
+        'dependent' => false
+      ),
+      'User' => array(
+        'className' => 'User',
+        'foreignKey' => 'timezone_id',
+        'dependent' => false
+      )
+    );
+
+  }
+  
 ?>
